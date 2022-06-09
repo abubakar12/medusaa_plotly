beec31f130ad1d061d991838f8333f878684bcf4
abubakar12 <abubakarilyas624@gmail.com>
2022-06-09 17:41:11 +0500

    initial bravery

--- products_per_unq_customers.py
+++ products_per_unq_customers.py
@@ -16,37 +16,18 @@ import plotly.io as pio
 pio.renderers.default='browser'
 from datetime import date, timedelta
 
+
+max_date=pd.to_datetime("2-18-2022",format="%m-%d-%Y")
+max_date=pd.to_datetime(max_date).date()
+# max_date=date.today()
+
+
 df = tableau_file.copy()  # iris is a pandas DataFrame
 df["product_type"]=df["product_type"].astype(str)
 df["CustomerID"]=df["CustomerID"].astype(str)
-df=df[["Date",'Year','Month',"Day",'CustomerID','product_type']]
+df=df[["Date",'Year','Month',"Day",'CustomerID','product_type','quantity']]
+
 df["Date"]=pd.to_datetime(df["Date"],format="%Y-%m-%d")
-df["unique_customer"]=df.groupby(["Date",'Year','Month',"Day",'CustomerID','product_type'])['CustomerID'].\
-    transform(lambda x:x.nunique())
-max_date=pd.to_datetime("2-18-2022",format="%m-%d-%Y")
-max_date=pd.to_datetime(max_date).date()
-max_date=date.today()
-
-
-less_than_current_date=[]
-for datee in df["Date"].unique():
-    datee=pd.to_datetime(datee).strftime('%Y-%m-%d')
-    datee=pd.to_datetime(datee,format="%Y-%m-%d").date()
-    if (datee<=max_date):
-        less_than_current_date.append(datee)
-        
-num_new_customers={}   
-new_customers=[]
-for datee in less_than_current_date:
-    print("date : {} ".format(str(datee)))
-    
-    customers=df[df["Date"]==np.datetime64(datee)]["CustomerID"].unique()
-    unique_customer_date=np.setdiff1d(customers,new_customers)
-    num_new_customers[str(datee)]=len(list(unique_customer_date))
-    new_customers=np.concatenate((new_customers, unique_customer_date), axis=None)
-    
-new_customers_tot=pd.DataFrame(num_new_customers.items(),columns=["Date","total_customers"])    
-new_customers_tot["Date"]=pd.to_datetime(new_customers_tot["Date"],format="%Y-%m-%d")
 
 less_than_30=d =max_date- timedelta(days=30)
 less_than_60=d =max_date- timedelta(days=60)
@@ -58,9 +39,7 @@ df.loc[df["Date"]>=pd.to_datetime(less_than_30),"less_than_30"]=1
 df.loc[df["Date"]>=pd.to_datetime(less_than_60),"less_than_60"]=1
 df.loc[df["Date"]>=pd.to_datetime(less_than_90),"less_than_90"]=1
 
-df=pd.merge(df,new_customers_tot,on=["Date"],how="left")
-
-
+df[df["less_than_90"]==1]
 
 
 
@@ -97,21 +76,34 @@ def display_(radio_value,days_prev):
     df_copy=df[df["product_type"]==radio_value].drop("product_type",1)
     
     if days_prev=='30':
-        df_copy=df_copy[df_copy["less_than_30"]==1].\
-            drop(["less_than_30","less_than_60","less_than_90"],1).drop_duplicates(subset=["Date"])
-    elif days_prev=='60':
-            df_copy=df_copy[df_copy["less_than_60"]==1].\
-                drop(["less_than_30","less_than_60","less_than_90"],1).drop_duplicates(subset=["Date"])
-                
-    elif days_prev=='90':
-            df_copy=df_copy[df_copy["less_than_90"]==1].\
-                drop(["less_than_30","less_than_60","less_than_90"],1).drop_duplicates(subset=["Date"])
-                
-    else:
-            df_copy=df_copy.drop(["less_than_30","less_than_60","less_than_90"],1).drop_duplicates(subset=["Date"])
+        df_copy=df_copy[df_copy["less_than_30"]==1]
+        df_copy["sum_qty"]=df_copy.groupby(['CustomerID'])['quantity'].transform(lambda x:x.sum())
+        df_copy["avg_product_per_customer"]=df_copy.groupby(['CustomerID'])['sum_qty'].\
+            transform(lambda x:x.mean())
+        df_copy=df_copy.drop_duplicates(subset=["CustomerID"])
             
+
+    elif days_prev=='60':
+        df_copy=df_copy[df_copy["less_than_60"]==1]
+        df_copy["sum_qty"]=df_copy.groupby(['CustomerID'])['quantity'].transform(lambda x:x.sum())
+        df_copy["avg_product_per_customer"]=df_copy.groupby(['CustomerID'])['sum_qty'].\
+            transform(lambda x:x.mean())
+        df_copy=df_copy.drop_duplicates(subset=["CustomerID"])
+         
+    elif days_prev=='90':
+        df_copy=df_copy[df_copy["less_than_90"]==1]
+        df_copy["sum_qty"]=df_copy.groupby(['CustomerID'])['quantity'].transform(lambda x:x.sum())
+        df_copy["avg_product_per_customer"]=df_copy.groupby(['CustomerID'])['sum_qty'].\
+            transform(lambda x:x.mean())
+        df_copy=df_copy.drop_duplicates(subset=["CustomerID"])
+         
+    else:
+        df_copy["sum_qty"]=df_copy.groupby(['CustomerID'])['quantity'].transform(lambda x:x.sum())
+        df_copy["avg_product_per_customer"]=df_copy.groupby(['CustomerID'])['sum_qty'].\
+            transform(lambda x:x.mean())
+        df_copy=df_copy.drop_duplicates(subset=["CustomerID"])    
     
-    fig = px.bar(df_copy, y="total_customers", x="Date")
+    fig = px.bar(df_copy, y="avg_product_per_customer", x="CustomerID")
 
     
     return fig
